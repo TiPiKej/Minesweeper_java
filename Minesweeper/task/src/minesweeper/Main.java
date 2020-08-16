4b28fe422c08d238951344c1e872ae9dbffdcb80
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-08-16 23:50:39 +0200

    Initial commit

---
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -0,0 +1,15 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".X.......\n" +
+                ".....X..X\n" +
+                "....X....\n" +
+                "......X..\n" +
+                "..X......\n" +
+                "....X....\n" +
+                "..X......\n" +
+                "..X......\n" +
+                "......X..");
+    }
+}
