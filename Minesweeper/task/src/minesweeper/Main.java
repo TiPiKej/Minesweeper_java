74678051331df8b2f760c84531589ea5407b736e
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-08-29 22:39:58 +0200

    .

--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -7,20 +7,34 @@ public class Main {
         final Scanner scanner = new Scanner(System.in);
 
         System.out.print("How many mines do you want on the field? ");
-        final Minesweeper minesweeper = new Minesweeper(scanner.nextInt());
+        final Minesweeper minesweeper = new Minesweeper(Integer.parseInt(scanner.nextLine()));
 
         System.out.println(minesweeper.toString());
-        while (!minesweeper.checkIfWin()) {
-            System.out.println("Set/delete mines marks (x and y coordinates):");
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
+        boolean win = true;
+        while (!minesweeper.checkIfEnd()) {
+            System.out.println("Set/unset mines marks or claim a cell as free:");
+            String[] line = scanner.nextLine().split(" ");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            int mode = 0;
+            switch (line[2]) {
+                case "free":
+                    mode = 1;
+                    break;
+                case "mine":
+                    mode = 2;
+                    break;
+            }
 
-            minesweeper.guess(y - 1, x - 1);
-            System.out.println(minesweeper.toString());
+            if (minesweeper.guess(y - 1, x - 1, mode) == 1) {
+                System.out.println(minesweeper.toString());
+                System.out.println("You stepped on a mine and failed!");
+                win = false;
+            } else System.out.println(minesweeper.toString());
         }
 
         scanner.close();
 
-        System.out.println("Congratulations! You found all mines!");
+        if (win) System.out.println("Congratulations! You found all mines!");
     }
 }
--- Minesweeper/task/src/minesweeper/Minesweeper.java
+++ Minesweeper/task/src/minesweeper/Minesweeper.java
@@ -12,6 +12,7 @@ public class Minesweeper {
     private int bombs;
     private int vote;
     private final List<Integer[]> bombsArr = new ArrayList<>();
+    private boolean steppedOnBomb = false;
 
     public Minesweeper(int width, int height, int bombs) {
         this.width = width;
@@ -42,7 +43,7 @@ public class Minesweeper {
 
     public void createField() {
 //        initialize
-        field = new int[height][width][3]; // third directional: isBomb(1 - yes, 0 - no), how much around him, if was guessed(1 - yes, 0 - no))
+        field = new int[height][width][4]; // third directional: [isBomb(1 - yes, 0 - no), how much around him, if was vote(1 - yes, 0 - no), if is marked(1 - yes, 0 - no)]
 
 //        fill with bombs
         for (int i = 0; i < bombs; i++) {
@@ -106,24 +107,60 @@ public class Minesweeper {
         return gen.nextInt(end - start + 1) + start;
     }
 
-    public int guess(int x, int y) {
-        System.out.println(x + "..." + y);
-        if (field[x][y][1] > 0 && field[x][y][0] == 0) {
+    private void checkPlace(int x, int y) {
+        if (field[x][y][2] == 1) return;
+
+        field[x][y][2] = 1;
+        field[x][y][3] = 0;
+        if (field[x][y][1] != 0) return;
+
+//        left
+        if (x > 0) checkPlace(x - 1, y);
+//        top
+        if (y > 0) checkPlace(x, y - 1);
+//        right
+        if (x < field.length - 1) checkPlace(x + 1, y);
+//        bottom
+        if (y < field.length - 1) checkPlace(x, y + 1);
+
+//        top left
+        if (x > 0 && y > 0) checkPlace(x - 1, y - 1);
+//        top right
+        if (x < field.length - 1 && y > 0) checkPlace(x + 1, y - 1);
+//        bottom left
+        if (x > 0 && y < field.length - 1) checkPlace(x - 1, y + 1);
+//        bottom right
+        if (x < field.length - 1 && y < field.length - 1) checkPlace(x + 1, y + 1);
+    }
+
+    public int guess(int x, int y, int mode) {
+        // third directional:
+        // [0: isBomb(1 - yes, 0 - no), 1: how much around him, 2: if was vote(1 - yes, 0 - no), 3: if is marked(1 - yes, 0 - no)]
+//        System.out.println(x + "..." + y);
+
+//        check if stepped on a mine
+        if (mode == 1 && field[x][y][0] == 1) {
+            steppedOnBomb = true;
             return 1;
         }
 
-        if (field[x][y][2] == 0) {
-            field[x][y][2] = 1;
-            vote++;
-        } else {
-            field[x][y][2] = 0;
-            vote--;
+//        draw / undraw mark
+        if (mode == 2) {
+            if (field[x][y][3] == 1) field[x][y][3] = 0;
+            else field[x][y][3] = 1;
+            return 0;
         }
+
+        if (mode != 1) return 2;
+
+        checkPlace(x, y);
 
         return 0;
     }
 
-    public boolean checkIfWin() {
+    public boolean checkIfEnd() {
+        if (steppedOnBomb) return true;
+
         if (vote != bombs) return false;
 
         for (Integer[] bomb : bombsArr) {
@@ -154,19 +191,23 @@ public class Minesweeper {
         ret.append(lineBreak);
 
 //        actually field
-        // third directional: [isBomb(1 - yes, 0 - no), how much around him, if was vote(1 - yes, 0 - no))]
+        // third directional: [0: isBomb(1 - yes, 0 - no), 1: how much around him, 2: if was vote(1 - yes, 0 - no), 3: if is marked(1 - yes, 0 - no)]
         for (int i = 0; i < height; i++) {
             ret.append((i + 1) + verticalSeparator);
             for (int j = 0; j < width; j++) {
-                ret.append(
-                        field[i][j][2] == 1 ?
-                                "*" :
-                                (field[i][j][1] == 0 ? "." : String.valueOf(field[i][j][1]))
-                );
-//                ret.append(field[i][j][0] == 1 ?
-//                        (field[i][j][1] == 0 ? "." : "*") :
-//                        (field[i][j][1] == 0 ? "." : String.valueOf(field[i][j][1]))
-//                );
+                if (steppedOnBomb && field[i][j][0] == 1) {
+                    ret.append("X");
+                } else if (field[i][j][3] == 1) {
+                    ret.append("*");
+                } else if (field[i][j][2] == 1) {
+                    if (field[i][j][1] == 0) {
+                        ret.append("/");
+                    } else {
+                        ret.append(field[i][j][1]);
+                    }
+                } else {
+                    ret.append(".");
+                }
             }
             ret.append(verticalSeparator + newLine);
         }
